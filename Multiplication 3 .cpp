5d44d7018bf93a84e8af8579c966d1cf68b222c4
Md. Al Amin <135636431+techtalent17@users.noreply.github.com>
2025-01-29 13:35:08 +0600

    Miltiplication
    Take 3 input and out put get by multiplication.

---
+++ Multiplication 3 .cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+    int main()     {
+    int x = 1;
+    int y = 10;
+    int m = 10;
+    int mul = x * y * m;
+     cout << mul;
+
+    return 0;
+    }
