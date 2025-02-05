d5adc77e7e79c4a87ea8d7ac72c325f00a14d086
Md. Al Amin <135636431+techtalent17@users.noreply.github.com>
2025-02-05 13:14:29 +0600

    Add files via upload
    Read an integer variable and print it

---
+++ Take input.cpp
@@ -0,0 +1,12 @@
+/*read an integer variable and print it*/
+
+#include <iostream>
+using namespace std;
+int main(){
+int num;
+cout << "Enter an integer num: " ;
+cin >> num;
+cout << "Integer number is: " << num;
+
+return 0;
+}
