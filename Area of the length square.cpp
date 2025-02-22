c1d88c6b8334b7166582d6251fa0f53892191e8d
Md. Al Amin <135636431+techtalent17@users.noreply.github.com>
2025-02-22 12:23:44 +0600

    Add files via upload

---
+++ Area of the length square.cpp
@@ -0,0 +1,16 @@
+//Area of the Square
+#include <iostream>
+using namespace std;
+
+int main() {
+    int L;
+    cin >> L;
+
+    // Calculate the area of the square
+    int area = L * L;
+
+    // Output the area
+    cout << area << endl;
+
+    return 0;
+}
